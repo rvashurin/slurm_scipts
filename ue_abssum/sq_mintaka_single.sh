792226ca47897679682cd93f8dc2abcf93df46f9
Roman Vashurin <rvashurin@gmail.com>
2023-02-01 13:02:54 +0500

    Add scripts for sq vs X

---
+++ ue_abssum/sq_mintaka_single.sh
@@ -0,0 +1,36 @@
+#!/bin/bash
+
+#SBATCH --job-name=sq_mintaka_single     # Job name
+
+#SBATCH --partition=gpu                      # queue name
+
+#SBATCH --mail-type=all                      # mail events (none, begin, end, fail, all)
+
+#SBATCH --mail-user=r.vashurin@skoltech.ru   # where to send mail
+
+#SBATCH --ntasks=1                           # run two task
+
+#SBATCH --cpus-per-task=12                    # number of cpu cores per task
+
+#SBATCH --mem=100000                          # job memory request in megabytes
+
+#SBATCH --gpus=1                             # number of gpus
+
+#SBATCH --time=02-00:00:00                   # time limit hrs:min:sec or dd-hrs:min:sec
+
+#SBATCH --output=/gpfs/gpfs0/r.vashurin/ue_abssum_logs/sq_mintaka_single.log
+
+
+#module load part
+
+#module load compilers/intel_2018.3.222 gpu/cuda-11.3
+
+module load python/anaconda3
+
+#command part
+
+source activate ue_abssum
+
+cd ~/codebases/ue_abssum
+
+HYDRA_CONFIG_NAME=ood_sq_mintaka_single DEVICE_FOR_DATA_RESTORING="cpu" python run_ue_t5.py
