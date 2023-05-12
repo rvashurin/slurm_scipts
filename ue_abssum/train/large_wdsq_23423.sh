a077d9a17740423c835abe28dbcb424cda87f582
Roman Vashurin <rvashurin@gmail.com>
2023-05-12 16:02:38 +0300

    Add new train scripts

---
+++ ue_abssum/train/large_wdsq_23423.sh
@@ -0,0 +1,36 @@
+#!/bin/bash
+
+#SBATCH --job-name=train_wdsq_large_23423          # Job name
+
+#SBATCH --partition=gpu                      # queue name
+
+#SBATCH --mail-type=all                      # mail events (none, begin, end, fail, all)
+
+#SBATCH --mail-user=r.vashurin@skoltech.ru   # where to send mail
+
+#SBATCH --ntasks=1                           # run two task
+
+#SBATCH --cpus-per-task=12                    # number of cpu cores per task
+
+#SBATCH --mem=70000                          # job memory request in megabytes
+
+#SBATCH --gpus=1                             # number of gpus
+
+#SBATCH --time=01-00:00:00                   # time limit hrs:min:sec or dd-hrs:min:sec
+
+#SBATCH --output=/gpfs/gpfs0/r.vashurin/ue_abssum_logs/train_wdsq_large_23423.log
+
+
+#module load part
+
+#module load compilers/intel_2018.3.222 gpu/cuda-11.3
+
+module load python/anaconda3
+
+#command part
+
+source activate ue_abssum
+
+cd ~/codebases/ue_abssum
+
+HYDRA_CONFIG_PATH=configs/t5/sp/with_prefix_tree HYDRA_CONFIG_NAME=train_t5_large_wdsq_23423 python train_t5.py
