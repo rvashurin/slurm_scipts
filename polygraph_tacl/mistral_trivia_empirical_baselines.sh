c46e399581cc3c910e83b412d245d40ceb352ddc
Roman Vashurin <rvashurin@gmail.com>
2024-08-30 16:25:50 +0400

    Add configs for empirical baseline calculations

---
+++ polygraph_tacl/mistral_trivia_empirical_baselines.sh
@@ -0,0 +1,32 @@
+#!/bin/bash
+
+#SBATCH --job-name=mistral_triviaqa_empirical_baselines
+
+#SBATCH --qos=gpu-12
+
+#SBATCH --partition=long                      # queue name
+
+#SBATCH --mail-type=all                      # mail events (none, begin, end, fail, all)
+
+#SBATCH --mail-user=Roman.Vashurin@mbzuai.ac.ae   # where to send mail
+
+#SBATCH --nodes=1
+
+#SBATCH --mem-per-cpu=100000                         # job memory request in megabytes
+
+#SBATCH --gres=gpu:4                             # number of gpus
+
+#SBATCH --time=00-24:00:00                   # time limit hrs:min:sec or dd-hrs:min:sec
+
+#SBATCH --output=/l/users/maxim.panov/log/tacl_instruct/mistral_triviaqa_empirical_baselines.log
+
+
+module load anaconda3
+
+#command part
+
+conda activate lm_polygraph_empirical
+
+cd /home/maxim.panov/workspace_vashurin/lm-polygraph-empirical
+
+HF_HOME=/l/users/maxim.panov/cache HYDRA_CONFIG=`pwd`/examples/configs/instruct/polygraph_eval_triviaqa_empirical_baselines.yaml polygraph_eval batch_size=4 cache_path=/l/users/maxim.panov/cache subsample_eval_dataset=2000
---
+++ polygraph_tacl/stable_coqa_empirical_baselines.sh
@@ -0,0 +1,32 @@
+#!/bin/bash
+
+#SBATCH --job-name=stable_coqa_empirical_baselines
+
+#SBATCH --qos=gpu-12
+
+#SBATCH --partition=long                      # queue name
+
+#SBATCH --mail-type=all                      # mail events (none, begin, end, fail, all)
+
+#SBATCH --mail-user=Roman.Vashurin@mbzuai.ac.ae   # where to send mail
+
+#SBATCH --nodes=1
+
+#SBATCH --mem-per-cpu=100000                         # job memory request in megabytes
+
+#SBATCH --gres=gpu:4                             # number of gpus
+
+#SBATCH --time=00-24:00:00                   # time limit hrs:min:sec or dd-hrs:min:sec
+
+#SBATCH --output=/l/users/maxim.panov/log/tacl_instruct/stable_coqa_empirical_baselines.log
+
+
+module load anaconda3
+
+#command part
+
+conda activate lm_polygraph_empirical
+
+cd /home/maxim.panov/workspace_vashurin/lm-polygraph-empirical
+
+HF_HOME=/l/users/maxim.panov/cache HYDRA_CONFIG=`pwd`/examples/configs/instruct/polygraph_eval_coqa_empirical_baselines.yaml polygraph_eval model=stablelm-2-12b-chat batch_size=4 cache_path=/l/users/maxim.panov/cache subsample_eval_dataset=2000
