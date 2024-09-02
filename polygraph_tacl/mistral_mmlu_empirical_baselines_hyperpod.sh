df13a62e4ad946f2db9dd6e1216e8511d49a227c
Roman Vashurin <rvashurin@gmail.com>
2024-09-02 17:37:45 +0400

    Add configs for empirical baselines calculations on hyperpod

---
+++ polygraph_tacl/mistral_mmlu_empirical_baselines_hyperpod.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+#SBATCH --job-name=mistral_mmlu_empirical_baselines
+#SBATCH --nodes=1                               # Run all processes on a single node
+#SBATCH --ntasks=1                              # Run on a single CPU
+#SBATCH --mem=40G                               # Total RAM to be used
+#SBATCH --cpus-per-task=8                       # Number of CPU cores
+#SBATCH --gres=gpu:1                            # Number of GPUs (per node)
+#SBATCH -p nlp-dept                             # Use the gpu partition
+#SBATCH -q nlp-pool                             # Use the gpu partition
+#SBATCH --time=48:00:00                         # Specify the time needed for your experiment
+#SBATCH --parsable
+#SBATCH --nodelist=gpu80-3,gpu80-2
+#SBATCH --output=/fsx/homes/Roman.Vashurin@mbzuai.ac.ae/logs/mistral_mmlu_empirical_baselines.log
+
+module load anaconda3
+
+#command part
+
+source activate lm_polygraph
+
+cd /home/maxim.panov/workspace/lm-polygraph
+
+HYDRA_CONFIG=`pwd`/examples/configs/instruct/polygraph_eval_mmlu_empirical_baselines.yaml polygraph_eval batch_size=2
