9b0d8574cada07e21ac63794db23610bc7e1831a
Roman Vashurin <rvashurin@gmail.com>
2024-11-15 16:01:14 +0400

    Add reverse langpairs scripts for wmt14 and llama 1b

---
+++ detr_ue_nmt/llama_3.2_3b/llama_3.2_1b_wmt14_encs.sh
@@ -0,0 +1,36 @@
+#!/bin/bash
+
+#SBATCH --job-name=llama_3.2_1b_wmt14_encs
+
+#SBATCH --qos=cscc-gpu-qos
+
+#SBATCH --partition=cscc-gpu-p                      # queue name
+
+#SBATCH --mail-type=all                      # mail events (none, begin, end, fail, all)
+
+#SBATCH --mail-user=Roman.Vashurin@mbzuai.ac.ae   # where to send mail
+
+#SBATCH --nodes=1
+
+#SBATCH --mem-per-cpu=100000                         # job memory request in megabytes
+
+#SBATCH --gres=gpu:1                             # number of gpus
+
+#SBATCH --time=02-00:00:00                   # time limit hrs:min:sec or dd-hrs:min:sec
+
+#SBATCH --output=/l/users/maxim.panov/log/detr_ue_nmt/llama_3.2_1b_wmt14_encs
+
+
+module load anaconda3
+
+#command part
+
+source activate lm_polygraph
+
+cd /home/maxim.panov/workspace_vashurin/lm-polygraph
+
+git checkout detr_ue_nmt
+
+pip install -e .
+
+HF_HOME=/l/users/maxim.panov/cache HYDRA_CONFIG=`pwd`/examples/configs/polygraph_eval_wmt14_encs.yaml polygraph_eval batch_size=1 cache_path=/l/users/maxim.panov/cache model=llama_1b subsample_eval_dataset=2000 deberta_batch_size=1
