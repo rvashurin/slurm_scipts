ea6b2e58006c33bdef210397891f56f70ff80afb
Roman Vashurin <rvashurin@gmail.com>
2024-11-28 11:55:22 +0400

    Add rest of sentsar configs

--- sar_with_maiya/ciai/mistral_trivia_sentsar_ciai.sh
+++ sar_with_maiya/ciai/mistral_trivia_sentsar_ciai.sh
@@ -10,13 +10,13 @@
 
 #SBATCH --mail-user=Roman.Vashurin@mbzuai.ac.ae   # where to send mail
 
-#SBATCH --nodes=2
+#SBATCH --nodes=1
 
 #SBATCH --mem-per-cpu=100000                         # job memory request in megabytes
 
 #SBATCH --gres=gpu:4                             # number of gpus
 
-#SBATCH --time=02-00:00:00                   # time limit hrs:min:sec or dd-hrs:min:sec
+#SBATCH --time=03-00:00:00                   # time limit hrs:min:sec or dd-hrs:min:sec
 
 #SBATCH --output=/l/users/maxim.panov/storage_vashurin/log/mistral_trivia_sentsar.log
 
@@ -29,7 +29,5 @@ source activate polygraph_maiya
 cd /home/maxim.panov/workspace_vashurin/polygraph_maiya
 
 git checkout sent_sar_variants
-
-pip install -e .
 
 PROTOCOL_BUFFERS_PYTHON_IMPLEMENTATION=python HF_HOME=/l/users/maxim.panov/cache HYDRA_CONFIG=`pwd`/examples/configs/polygraph_eval_triviaqa_sentsar.yaml polygraph_eval batch_size=1 cache_path=/l/users/maxim.panov/storage_vashurin/polygraph_out subsample_eval_dataset=2000 deberta_batch_size=1 model.path=mistral-community/Mistral-7B-v0.2
