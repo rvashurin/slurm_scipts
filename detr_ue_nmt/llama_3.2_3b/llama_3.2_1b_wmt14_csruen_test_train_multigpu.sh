766542a1578412a81e30d6e2115e9215496a7a99
Roman Vashurin <rvashurin@gmail.com>
2025-02-21 14:32:49 +0400

    Move some things around

--- detr_ue_nmt/llama_3.2_3b/llama_3.2_1b_wmt14_csruen_test_train_multigpu.sh
+++ detr_ue_nmt/llama_3.2_3b/llama_3.2_1b_wmt14_csruen_test_train_multigpu.sh
@@ -12,7 +12,9 @@
 
 #SBATCH --nodes=1
 
-#SBATCH --mem-per-cpu=200000                         # job memory request in megabytes
+#SBATCH --mem-per-cpu=20000                         # job memory request in megabytes
+
+#SBATCH --cpus-per-task=10
 
 #SBATCH --gres=gpu:4                             # number of gpus
 
@@ -33,12 +35,13 @@ git checkout detr_ue_nmt
 
 pip install -e .
 
-HF_HOME=/l/users/maxim.panov/cache HYDRA_CONFIG=`pwd`/examples/configs/polygraph_eval_wmt14_csen.yaml polygraph_eval batch_size=1 cache_path=/l/users/maxim.panov/cache model=llama_1b subsample_eval_dataset=2000 deberta_batch_size=1 +deberta_device=cuda:0 model.load_model_args.device_map=cuda:0 > /l/users/maxim.panov/log/detr_ue_nmt/llama_3.2_1b_wmt14_csruen_test_train_multigpu_csen.log 2>&1 &
 
-HF_HOME=/l/users/maxim.panov/cache HYDRA_CONFIG=`pwd`/examples/configs/polygraph_eval_wmt14_csen.yaml polygraph_eval batch_size=1 cache_path=/l/users/maxim.panov/cache model=llama_1b subsample_eval_dataset=2000 deberta_batch_size=1 +deberta_device=cuda:1 model.load_model_args.device_map=cuda:1 eval_split=train > /l/users/maxim.panov/log/detr_ue_nmt/llama_3.2_1b_wmt14_csruen_test_train_multigpu_csen_train.log 2>&1 &
+TOKENIZERS_PARALLELISM=true OPENBLAS_MAIN_FREE=1 HF_HOME=/l/users/maxim.panov/cache HYDRA_CONFIG=`pwd`/examples/configs/polygraph_eval_wmt14_csen.yaml polygraph_eval batch_size=1 cache_path=/l/users/maxim.panov/cache model=llama_1b subsample_eval_dataset=2000 deberta_batch_size=1 +deberta_device=cuda:0 model.load_model_args.device_map=cuda:0 > /l/users/maxim.panov/log/detr_ue_nmt/llama_3.2_1b_wmt14_csruen_test_train_multigpu_csen.log 2>&1 &
 
-HF_HOME=/l/users/maxim.panov/cache HYDRA_CONFIG=`pwd`/examples/configs/polygraph_eval_wmt14_ruen.yaml polygraph_eval batch_size=1 cache_path=/l/users/maxim.panov/cache model=llama_1b subsample_eval_dataset=2000 deberta_batch_size=1 +deberta_device=cuda:2 model.load_model_args.device_map=cuda:2 > /l/users/maxim.panov/log/detr_ue_nmt/llama_3.2_1b_wmt14_csruen_test_train_multigpu_ruen.log 2>&1 &
+TOKENIZERS_PARALLELISM=true OPENBLAS_MAIN_FREE=1 HF_HOME=/l/users/maxim.panov/cache HYDRA_CONFIG=`pwd`/examples/configs/polygraph_eval_wmt14_csen.yaml polygraph_eval batch_size=1 cache_path=/l/users/maxim.panov/cache model=llama_1b subsample_eval_dataset=2000 deberta_batch_size=1 +deberta_device=cuda:1 model.load_model_args.device_map=cuda:1 eval_split=train > /l/users/maxim.panov/log/detr_ue_nmt/llama_3.2_1b_wmt14_csruen_test_train_multigpu_csen_train.log 2>&1 &
 
-HF_HOME=/l/users/maxim.panov/cache HYDRA_CONFIG=`pwd`/examples/configs/polygraph_eval_wmt14_ruen.yaml polygraph_eval batch_size=1 cache_path=/l/users/maxim.panov/cache model=llama_1b subsample_eval_dataset=2000 deberta_batch_size=1 +deberta_device=cuda:3 model.load_model_args.device_map=cuda:3 eval_split=train > /l/users/maxim.panov/log/detr_ue_nmt/llama_3.2_1b_wmt14_csruen_test_train_multigpu_ruen_train.log 2>&1 &
+TOKENIZERS_PARALLELISM=true OPENBLAS_MAIN_FREE=1 HF_HOME=/l/users/maxim.panov/cache HYDRA_CONFIG=`pwd`/examples/configs/polygraph_eval_wmt14_ruen.yaml polygraph_eval batch_size=1 cache_path=/l/users/maxim.panov/cache model=llama_1b subsample_eval_dataset=2000 deberta_batch_size=1 +deberta_device=cuda:2 model.load_model_args.device_map=cuda:2 > /l/users/maxim.panov/log/detr_ue_nmt/llama_3.2_1b_wmt14_csruen_test_train_multigpu_ruen.log 2>&1 &
+
+TOKENIZERS_PARALLELISM=true OPENBLAS_MAIN_FREE=1 HF_HOME=/l/users/maxim.panov/cache HYDRA_CONFIG=`pwd`/examples/configs/polygraph_eval_wmt14_ruen.yaml polygraph_eval batch_size=1 cache_path=/l/users/maxim.panov/cache model=llama_1b subsample_eval_dataset=2000 deberta_batch_size=1 +deberta_device=cuda:3 model.load_model_args.device_map=cuda:3 eval_split=train > /l/users/maxim.panov/log/detr_ue_nmt/llama_3.2_1b_wmt14_csruen_test_train_multigpu_ruen_train.log 2>&1 &
 
 wait
--- sar_with_maiya/ciai/cp_for_llama.py
+++ sar_with_maiya/ciai/cp_for_llama.py
@@ -2,16 +2,16 @@ import os
 
 # iterate over files with .sh extension in current directory
 for file in os.listdir('.'):
-    if file.endswith('.sh'):
+    if file.endswith('.sh') and file.startswith('mistral'):
         # open file and read contents
         with open(file, 'r') as f:
             content = f.read()
             # replace mistral to llama in contents
 
-            content = content.replace('model.path=mistral-community/Mistral-7B-v0.2', 'model=llama')
-            content = content.replace('mistral', 'llama')
+            content = content.replace('model.path=mistral-community/Mistral-7B-v0.2', 'model=falcon3')
+            content = content.replace('mistral', 'falcon')
 
             # write contents to new file replacing mistral with llama in filename
-            with open(file.replace('mistral', 'llama'), 'w') as f:
+            with open(file.replace('mistral', 'falcon'), 'w') as f:
                 f.write(content)
 
