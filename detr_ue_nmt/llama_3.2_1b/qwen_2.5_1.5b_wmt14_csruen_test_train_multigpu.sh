fea6632694d78ee3410ca5c13b3bbfbb072fa45b
Roman Vashurin <rvashurin@gmail.com>
2025-02-26 13:47:54 +0400

    Add configs for qwen and falcon

---
+++ detr_ue_nmt/llama_3.2_1b/qwen_2.5_1.5b_wmt14_csruen_test_train_multigpu.sh
@@ -0,0 +1,47 @@
+#!/bin/bash
+
+#SBATCH --job-name=llama_3.2_1b_wmt14_csruen_test_train_multigpu
+
+#SBATCH --qos=cscc-gpu-qos
+
+#SBATCH --partition=long                      # queue name
+
+#SBATCH --mail-type=all                      # mail events (none, begin, end, fail, all)
+
+#SBATCH --mail-user=Roman.Vashurin@mbzuai.ac.ae   # where to send mail
+
+#SBATCH --nodes=1
+
+#SBATCH --mem-per-cpu=20000                         # job memory request in megabytes
+
+#SBATCH --cpus-per-task=10
+
+#SBATCH --gres=gpu:4                             # number of gpus
+
+#SBATCH --time=03-00:00:00                   # time limit hrs:min:sec or dd-hrs:min:sec
+
+#SBATCH --output=/l/users/maxim.panov/log/detr_ue_nmt/llama_3.2_1b_wmt14_csruen_test_train_multigpu
+
+
+module load anaconda3
+
+#command part
+
+source activate lm_polygraph
+
+cd /home/maxim.panov/workspace_vashurin/lm-polygraph
+
+git checkout detr_ue_nmt
+
+pip install -e .
+
+
+TOKENIZERS_PARALLELISM=true OPENBLAS_MAIN_FREE=1 HF_HOME=/l/users/maxim.panov/cache HYDRA_CONFIG=`pwd`/examples/configs/polygraph_eval_wmt14_csen.yaml polygraph_eval batch_size=1 cache_path=/l/users/maxim.panov/cache model=llama_1b model.path=Qwen/Qwen2.5-1.5B subsample_eval_dataset=2000 deberta_batch_size=1 +deberta_device=cuda:0 model.load_model_args.device_map=cuda:0 > /l/users/maxim.panov/log/detr_ue_nmt/llama_3.2_1b_wmt14_csruen_test_train_multigpu_csen.log 2>&1 &
+
+TOKENIZERS_PARALLELISM=true OPENBLAS_MAIN_FREE=1 HF_HOME=/l/users/maxim.panov/cache HYDRA_CONFIG=`pwd`/examples/configs/polygraph_eval_wmt14_csen.yaml polygraph_eval batch_size=1 cache_path=/l/users/maxim.panov/cache model=llama_1b model.path=Qwen/Qwen2.5-1.5B subsample_eval_dataset=2000 deberta_batch_size=1 +deberta_device=cuda:1 model.load_model_args.device_map=cuda:1 eval_split=train > /l/users/maxim.panov/log/detr_ue_nmt/llama_3.2_1b_wmt14_csruen_test_train_multigpu_csen_train.log 2>&1 &
+
+TOKENIZERS_PARALLELISM=true OPENBLAS_MAIN_FREE=1 HF_HOME=/l/users/maxim.panov/cache HYDRA_CONFIG=`pwd`/examples/configs/polygraph_eval_wmt14_ruen.yaml polygraph_eval batch_size=1 cache_path=/l/users/maxim.panov/cache model=llama_1b model.path=Qwen/Qwen2.5-1.5B subsample_eval_dataset=2000 deberta_batch_size=1 +deberta_device=cuda:2 model.load_model_args.device_map=cuda:2 > /l/users/maxim.panov/log/detr_ue_nmt/llama_3.2_1b_wmt14_csruen_test_train_multigpu_ruen.log 2>&1 &
+
+TOKENIZERS_PARALLELISM=true OPENBLAS_MAIN_FREE=1 HF_HOME=/l/users/maxim.panov/cache HYDRA_CONFIG=`pwd`/examples/configs/polygraph_eval_wmt14_ruen.yaml polygraph_eval batch_size=1 cache_path=/l/users/maxim.panov/cache model=llama_1b model.path=Qwen/Qwen2.5-1.5B subsample_eval_dataset=2000 deberta_batch_size=1 +deberta_device=cuda:3 model.load_model_args.device_map=cuda:3 eval_split=train > /l/users/maxim.panov/log/detr_ue_nmt/llama_3.2_1b_wmt14_csruen_test_train_multigpu_ruen_train.log 2>&1 &
+
+wait
---
+++ detr_ue_nmt/llama_3.2_1b/qwen_falcon.py
@@ -0,0 +1,27 @@
+# for each file that has 'train_multigpu.sh' in the name, find the lines where substring 'model=llama_1b' is present and add the following after the substring: ' model.path=Qwen/Qwen2.5-1.5B'
+
+import os
+
+models = {
+    'qwen_2.5_1.5b': 'Qwen/Qwen2.5-1.5B',
+    'falcon_3_1b': 'tiiuae/Falcon3-1B-Base'
+}
+
+def main():
+    path = '.'
+    for root, dirs, files in os.walk(path):
+        for file in files:
+            if 'train_multigpu.sh' in file:
+                file_path = os.path.join(root, file)
+                with open(file_path, 'r') as f:
+                    lines = f.readlines()
+                for model, model_path in models.items():
+                    new_file_path = file_path.replace('llama_3.2_1b', model)
+                    with open(new_file_path, 'w') as f:
+                        for line in lines:
+                            if 'model=llama_1b' in line:
+                                line = line.replace('model=llama_1b', 'model=llama_1b model.path=' + model_path)
+                            f.write(line)
+
+if __name__ == '__main__':
+    main()
